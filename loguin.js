67bd03693246627e8172d8ccc8c7985105506278
lalasa30320805 <134504046+lalasa30320805@users.noreply.github.com>
2023-09-26 08:40:59 -0300

    Add files via upload

---
+++ loguin.js
@@ -0,0 +1,13 @@
+lgn = "lahsoh";
+pwd = "123456";
+
+function autenticar (){
+var log = document.getElementById('login').value;
+var pass = document.getElementById('passwd').value;
+
+if (log == lgn & pass == pwd){
+    alert ('fon')
+}else{
+    alert ('firifonfon')
+}
+}
